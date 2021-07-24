985dfdfe330a17da473b9be70cf96d79daea385a
Daniel Brooks <bass_rock@me.com>
2021-07-24 00:59:43 -0700

    Update hackintosh.sh

--- asrock x570 phantom gaming itx tb3 (proxintosh)/proxmox-host/var/lib/vz/snippets/hackintosh.sh
+++ asrock x570 phantom gaming itx tb3 (proxintosh)/proxmox-host/var/lib/vz/snippets/hackintosh.sh
@@ -4,30 +4,18 @@ if [ "$2" == "pre-start" ]
 then
 # First release devices from their current driver (by their PCI bus IDs)
 #wifi/bt
-echo 0000:25:00.0 > /sys/bus/pci/devices/0000:25:00.0/driver/unbind
+echo vfio-pci > /sys/bus/pci/devices/0000\:25\:00.0/driver_override
+
 #usb
-echo 0000:27:00.1 > /sys/bus/pci/devices/0000:27:00.1/driver/unbind
-echo 0000:27:00.3 > /sys/bus/pci/devices/0000:27:00.3/driver/unbind
-echo 0000:2e:00.3 > /sys/bus/pci/devices/0000:2e:00.3/driver/unbind
-echo 0000:2e:00.4 > /sys/bus/pci/devices/0000:2e:00.4/driver/unbind
-#vega
-echo 0000:2c:00.0 > /sys/bus/pci/devices/0000:2c:00.0/driver/unbind
-echo 0000:2c:00.1 > /sys/bus/pci/devices/0000:2c:00.1/driver/unbind
-#nvme
-echo 0000:03:00.0 > /sys/bus/pci/devices/0000:03:00.0/driver/unbind
-
-# Then attach them by ID to VFIO
-#wifi/bt
-echo 14e4 43a0 > /sys/bus/pci/drivers/vfio-pci/new_id
-
-#usb controllsers
-echo 1022 149c > /sys/bus/pci/drivers/vfio-pci/new_id
+echo vfio-pci > /sys/bus/pci/devices/0000\:27\:00.1/driver_override
+echo vfio-pci > /sys/bus/pci/devices/0000\:27\:00.3/driver_override
+echo vfio-pci > /sys/bus/pci/devices/0000\:2e\:00.3/driver_override
+echo vfio-pci > /sys/bus/pci/devices/0000\:2e\:00.4/driver_override
 
 #vega
-echo 1002 687f > /sys/bus/pci/drivers/vfio-pci/new_id
-echo 1002 aaf8 > /sys/bus/pci/drivers/vfio-pci/new_id
+echo vfio-pci > /sys/bus/pci/devices/0000\:2c\:00.0/driver_override
+echo vfio-pci > /sys/bus/pci/devices/0000\:2c\:00.1/driver_override
 
 #nvme
-
-echo 1987 5012 > /sys/bus/pci/drivers/vfio-pci/new_id
+echo vfio-pci > /sys/bus/pci/devices/0000\:03\:00.0/driver_override
 fi
